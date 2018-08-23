82659116f9022e880f8d9abe582eaa93ba51d168
wanglong <ant0x00@163.com>
2018-08-23 15:43:48 +0800

    增加快速排序

--- sortingPrimary.py
+++ sortingPrimary.py
@@ -24,6 +24,11 @@ def bubble_sorting():
 
 
 def fast_sorting(test_list):
+    """
+    快速排序：
+    思路：找一个基准元素N，然后先从右往左找第一个小于N的元素S，再由左往右找大于N的元素L，将S和L对调，再寻找，重复对调
+          知道左右的坐标相遇，然后和N对调，递归。
+    """
     less = []
     pivotList = []
     more = []
@@ -43,9 +48,18 @@ def fast_sorting(test_list):
         more = fast_sorting(more)
         return less + pivotList + more
 
+
+def qsort(arr):
+    """快排更简洁的写法"""
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        return qsort([x for x in arr[1:] if x < pivot]) + [pivot] + qsort([x for x in arr[1:] if x >= pivot])
+
 if __name__ == "__main__":
     test_list = [26, 19, 27, 80, -9, 17, 37, 25, 18]
-    print(fast_sorting(test_list))
+    print(qsort(test_list))
     # buket_sorting()
     # bubble_sorting()
     # bubble_sorting()
