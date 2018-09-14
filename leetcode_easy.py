ae54ea8c1be3379012206aa7d944af4efe6f68c5
wanglong <ant0x00@163.com>
2018-09-14 16:21:27 +0800

    - 最长公共前缀很pythonic的解法 - 增加注释

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -1,7 +1,14 @@
+# coding: utf-8
+
 def reverse(x):
     """
     :type x: int
     :rtype: int
+    反转整数
+    思路：
+    1. 判断正负数
+    2. 从左到右取值
+    3. 返回时判断反转数是否溢出
     """
     sign = x < 0 and -1 or 1
     x = abs(x)
@@ -10,10 +17,11 @@ def reverse(x):
         ans = ans * 10 + x % 10
         x //= 10
         # x = int(x/10)
-    return sign * ans if ans <= 0x7fffffff else 0
+    return sign * ans if ans <= 0x7fffffff else 0 #0x7fffffff=2**32-1
 
 
 def is_palindrome_number(x):
+    # 判断是否回文数
     if x < 0 or (x % 10 == 0 and x != 0):
         return False
     reverse_number = 0
@@ -25,6 +33,7 @@ def is_palindrome_number(x):
 
 def romanToInt(s):
     # https://leetcode-cn.com/problems/roman-to-integer/description/
+    # 罗马数字转整数
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     ans = 0
     for i in range(len(s) - 1):
@@ -39,6 +48,9 @@ def romanToInt(s):
 
 
 def longestCommonPrefix(strs):
+    # https://leetcode-cn.com/problems/longest-common-prefix/description/
+    # 最长公共前缀
+    # 还有其他解法 https://blog.csdn.net/ojshilu/article/details/12882835
     if not strs:
         return ""
     strs.sort(key=len)
@@ -51,10 +63,22 @@ def longestCommonPrefix(strs):
                 break
     return shortest_str
 
-
+def longestCommonPrefix_use_zip(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    最长公共前缀很pythonic的解法
+    """
+    prefix = ''
+    for item in list(zip(*strs)):
+        if len(set(item)) > 1:
+            return prefix
+        else:
+            prefix += item[0]
+    return prefix
 
 if __name__ == '__main__':
     # print(reverse(-3210))
     # print(is_palindrome_number(121))
     # print(romanToInt("LVIII"))
-    print(longestCommonPrefix(["abca","aba","aaab"]))
+    print(longestCommonPrefix_use_zip(["abca", "aba", "abab"]))
