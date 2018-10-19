0ac51b35c38813265800317ddad5fdd529ce3797
wanglong <ant0x00@163.com>
2018-10-19 17:31:35 +0800

    344 - 反转字符串

---
+++ 344_reverseString.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        双指针使用（left和right）
+        """
+        if not s:
+            return ''
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+        while right > left:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return ''.join(s)
+
+    def reverseString2(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ''.join([x for x in reversed(s)])
+
+
+def stringToString(input):
+    return input[1:-1]
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            s = stringToString(line);
+
+            ret = Solution().reverseString(s)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
