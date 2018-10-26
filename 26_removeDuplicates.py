1345c4760ee471334bc812570c559039c96ccbc1
wanglong <ant0x00@163.com>
2018-10-26 17:52:25 +0800

    - 寻找多数问题 - 删除排序数组中的重复项

---
+++ 26_removeDuplicates.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+import json
+
+
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
+        begin = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[begin]:
+                begin += 1
+                nums[begin] = nums[i]
+        return begin + 1
+
+        # if len(nums) <= 1:
+        #     return len(nums)
+        # slow = 0
+        # for i in xrange(1, len(nums)):
+        #     if nums[i] != nums[slow]:
+        #         slow += 1
+        #         nums[slow] = nums[i]
+        # return slow + 1
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line)
+
+            ret = Solution().removeDuplicates(nums)
+
+            out = integerListToString(nums, len_of_list=ret)
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ find_majority_num.py
@@ -0,0 +1,33 @@
+def condidate(m, A):
+    j = m
+    c = A[m]
+    count = 1
+    while j < len(A) - 1 and count > 0:
+        j = j + 1
+        if A[j] == c:
+            count = count + 1
+        else:
+            count = count - 1
+    if j == len(A) - 1:
+        return c
+    else:
+        return condidate(j + 1, A)
+
+
+
+def majority(A):
+    c = condidate(0, A)
+    count = 0
+    for i in range(len(A)):
+        if A[i] == c:
+            count = count + 1
+    if count > len(A) / 2:
+        return c
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    data = [1,2,1,2]
+    result = majority(data)
+    print(result)
